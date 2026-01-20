d41086bce5c0257281c92088113746f35e0d2031
cl4pper <cl4pper@gmail.com>
2026-01-20 20:19:04 -0300

    feat: access array values

--- python/data_analytics/guide_numpy.py
+++ python/data_analytics/guide_numpy.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Initialize
 country_names = ['Brazil', 'Germany', 'Russia']
-coordinates = [[41.56, 40.48], [36.50, 41.37]]
+coordinates = [[41.56, 40.48], [36.51, 41.37]]
 
 # Get dimension & shape
 class ArrayElement:
@@ -46,3 +46,10 @@ array_country_names = ArrayElement('country_names', country_names)
 print(array_country_names.get_config())
 array_coodinates = ArrayElement('coordinates', coordinates)
 print(array_coodinates.get_config())
+
+# Accessing rows, columns, etc
+# arr[row, column]
+array_int = [1,2,3,4,5,6,7,8,9]
+print(f"first row from coordinates {array_coodinates.get_self()[0, :]}")
+print(f"first column from coordinates {array_coodinates.get_self()[:, 0]}")
+# array[2, 2:8:2] -> get elements on row 2, from the element 2 until element 8, stepping by 2
